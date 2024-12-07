9b71b2620644ec0c031a1d7df5f46970b7833a65
m3b1u5 <dbatezat@gmail.com>
2024-12-07 14:16:31 +0200

    ДЗ: Добавление книги

--- main.py
+++ main.py
@@ -3,12 +3,41 @@
 
 def book_list_view(library):
     if not library:
-        print("Библиотека пуста")
+        print("\nБиблиотека пуста")
     else:
-        print("Список книг по названию:\n" + '_' * 50)
+        print("\nСписок книг по названию:\n" + '_' * 50)
         for book in library:
             print(book)
         print('_' * 50)
+
+
+def update_book(title, author, year, library):
+    while True:
+        try:
+            update_required = input("\nКнига уже есть в списке, обновить информацию (Да \ Нет)?: ").lower()
+            if update_required == 'да':
+                library[title]['author'] = author
+                library[title]['year'] = year
+                library[title]['present'] = None
+                print(f"Информация о книге \"{title}\" обновлена.")
+                return
+            elif update_required == 'нет':
+                print(f"Информация о книге \"{title}\" оставлена без изменений.")
+                return
+            else:
+                raise ValueError
+        except ValueError:
+            print("Введите Да или Нет")
+
+
+def add_book(title, author, year, library):
+    if title in library:
+        update_book(title, author, year, library)
+    else:
+        library[title]['author'] = author
+        library[title]['year'] = year
+        library[title]['present'] = None
+        print(f"\nИнформация о книге \"{title}\" успешно добавлена в список библиотеки")
 
 
 def main():
@@ -18,6 +47,8 @@ def main():
         "Красненькая такая": {"author": "Автор", "year": 1, "present": True},
     }
 
+    book_list_view(library)
+    add_book('Словарь', 'Я', 1999, library)
     book_list_view(library)
 
 
